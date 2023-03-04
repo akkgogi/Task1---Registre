6fbd3e8dcfb1d17f202c2bfa902478ce689b017a
akkgogi <akkgogi@yandex.ru>
2023-03-05 02:12:35 +0300

    Добавил поддержку 'ё'

--- SobesRegistre2/Program.cs
+++ SobesRegistre2/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string str;
+string strres = "";
+char cres;
+
+Console.WriteLine("Введите строку:");
+str = Console.ReadLine();
+
+foreach (char c in str)
+{
+    if (Char.IsLetter(c))
+    {
+        if (c == 'ё')
+        {
+            cres = (char)((int)c - 80);
+        }
+        else if (c == 'Ё')
+        {
+            cres = (char)((int)c + 80);
+        }
+        else if (Char.IsUpper(c))
+        {
+            cres = (char)((int)c + 32);
+        }
+        else
+        {
+            cres = (char)((int)c - 32);
+        }
+        strres += cres;
+    }
+    else
+    {
+        strres += c;
+    }
+
+}
+
+Console.WriteLine(strres);
