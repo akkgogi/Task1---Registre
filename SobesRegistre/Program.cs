01daed49e660583762aef8f87ba3c73cb33a44f8
akkgogi <akkgogi@yandex.ru>
2023-03-05 01:18:16 +0300

    Русский алфавит

--- SobesRegistre/Program.cs
+++ SobesRegistre/Program.cs
@@ -3,22 +3,36 @@ string strres = "";
 
 string alphabetMajor = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 string alphabetMinor = "abcdefghijklmnopqrstuvwxyz";
+string alphabetMajorRus = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+string alphabetMinorRus = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
 
 Console.WriteLine("Введите строку:");
 str = Console.ReadLine();
 
 foreach(char c in str)
 {
-    if ((int)c > 64 & (int)c < 91)
+    if ((int)c >= (int)alphabetMajor.First() & (int)c <= (int)alphabetMajor.Last())
     {
         int index = alphabetMajor.IndexOf(c);
         char cres = alphabetMinor[index];
         strres += cres;
     }
-    else if ((int)c > 96 & (int)c < 123)
+    else if ((int)c >= (int)alphabetMinor.First() & (int)c <= (int)alphabetMinor.Last())
     {
         int index = alphabetMinor.IndexOf(c);
         char cres = alphabetMajor[index];
+        strres += cres;
+    }
+    else if ((int)c >= (int)alphabetMajorRus.First() & (int)c <= (int)alphabetMajorRus.Last())
+    {
+        int index = alphabetMajorRus.IndexOf(c);
+        char cres = alphabetMinorRus[index];
+        strres += cres;
+    }
+    else if ((int)c >= (int)alphabetMinorRus.First() & (int)c <= (int)alphabetMinorRus.Last())
+    {
+        int index = alphabetMinorRus.IndexOf(c);
+        char cres = alphabetMajorRus[index];
         strres += cres;
     }
     else
