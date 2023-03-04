3c10c1d40f09d0814cf8edfc3fccada80619ec7f
akkgogi <akkgogi@yandex.ru>
2023-03-05 01:39:04 +0300

    Второе решение

---
+++ SobesRegistre2/Program.cs
@@ -0,0 +1,29 @@
+﻿string str;
+string strres = "";
+char cres;
+
+Console.WriteLine("Введите строку:");
+str = Console.ReadLine();
+
+foreach (char c in str)
+{
+    if (Char.IsLetter(c))
+    {
+        if (Char.IsUpper(c))
+        {
+            cres = (char)((int)c + 32);
+        }
+        else
+        {
+            cres = (char)((int)c - 32);
+        }
+        strres += cres;
+    }
+    else
+    {
+        strres += c;
+    }
+
+}
+
+Console.WriteLine(strres);
